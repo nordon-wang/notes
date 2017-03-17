aac11a7cf9405ada44b99a526f3287fdd16f3207
nordon-wang <1531959404@qq.com>
2017-03-17 09:58:42 +0800

    new

---
+++ vue/javascript/animate.js
@@ -0,0 +1,26 @@
+new Vue({
+	el: "#app1",
+	data() {
+		return {
+			show: true
+		}
+	}
+})
+
+new Vue({
+	el: "#app2",
+	data() {
+		return {
+			show: true
+		}
+	}
+})
+
+var app3 = new Vue({
+	el: '#app3',
+	data() {
+		return {
+			isEdit: true
+		}
+	}
+})
