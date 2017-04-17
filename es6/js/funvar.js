da3dfdb433f7870792c254b23e6ec6d622843b1f
nordon-wang <1531959404@qq.com>
2017-04-17 09:37:36 +0800

    new

---
+++ es6/js/funvar.js
@@ -0,0 +1,12 @@
+// console.log('1111',a);
+// var a = 3;
+// function a() { }
+
+function a() { }
+var a;
+console.log('2222',a);
+a = 3;
+
+// function a() { }
+// console.log('33333', a);
+// var a = 3;
